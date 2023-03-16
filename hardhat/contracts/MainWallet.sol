dde13958a0705f4d92f5f17d2216dfde971048cc
Wade <wz.lim.00@gmail.com>
2023-03-16 18:26:40 +0800

    cleaned code

--- hardhat/contracts/MainWallet.sol
+++ hardhat/contracts/MainWallet.sol
@@ -15,27 +15,27 @@ contract MainWallet is SimpleAccount {
         uint256 lastPaymentBlock;
     }
     
-    mapping(address => ReccuringPayee) public reccuringPayments;
+    mapping(address => ReccuringPayeeInfo) public reccuringPayments;
     // address[] public socialRecoveryAccounts;
     // address public attestationStation;
 
-    // constructor(address[] calldata _recoveryAccounts) {
-
-    // }
+    constructor(IEntryPoint anEntryPoint /*, address[] calldata _recoveryAccounts*/) SimpleAccount(anEntryPoint) {
+    }
 
     // Used off-chain to check if payment is needed
-    function isPaymentNeeded(address _payeeWallet) view returns (bool) {
+    function isPaymentNeeded(address _payeeWallet) public view returns (bool) {
         ReccuringPayeeInfo memory info = reccuringPayments[_payeeWallet];
         if (info.paymentAmount > 0) {
-            return (info.lastPaymentBlock + period) > block.number;
+            return (info.lastPaymentBlock + info.period) > block.number;
         } else {
             return false;
         }
     }
 
     // Used to make reccuring payment
-    function makeReccuringPayment(address _payeeWallet) view returns (bool) {
+    function makeReccuringPayment(address _payeeWallet) public {
         require(isPaymentNeeded(_payeeWallet), "payment not made");
+        
         ReccuringPayeeInfo memory info = reccuringPayments[_payeeWallet];
         IERC20(info.token).transfer(_payeeWallet, info.paymentAmount);
         reccuringPayments[_payeeWallet].lastPaymentBlock += block.number;
