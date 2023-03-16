ed0ffd7c2f20056d93e19cd9ebaffbbb5008409d
Wade <wz.lim.00@gmail.com>
2023-03-16 18:42:06 +0800

    added recovery

--- hardhat/contracts/MainWallet.sol
+++ hardhat/contracts/MainWallet.sol
@@ -17,9 +17,33 @@ contract MainWallet is SimpleAccount {
     
     mapping(address => ReccuringPayeeInfo) public reccuringPayments;
     // address[] public socialRecoveryAccounts;
-    // address public attestationStation;
+    address public attestationStation;
+    address public recoveryAccount1;
+    address public recoveryAccount2;
+    address public recoveryAccount3;
+    address public newAccount; // new wallet if user lost the current wallet
 
-    constructor(IEntryPoint anEntryPoint /*, address[] calldata _recoveryAccounts*/) SimpleAccount(anEntryPoint) {
+
+    constructor(IEntryPoint anEntryPoint, address _attestationStation) SimpleAccount(anEntryPoint) {
+        attestationStation = _attestationStation;
+    }
+
+    // only wallet owner can call
+    function setUpReccuringPayment(
+        address _payeeWallet, 
+        uint256 _amount, 
+        address _token, 
+        uint256 _period, 
+        address _firstPaymentBlock
+    ) public {
+        ReccuringPayeeInfo memory newInfo;
+        newInfo.paymentAmount = _amount;
+        newInfo.token = _token;
+        newInfo.period = period;
+        newInfo.lastPaymentBlock - _firstPaymentBlock;
+
+        reccuringPayments[_payeeWallet] = newInfo;
+        // emit some event
     }
 
     // Used off-chain to check if payment is needed
@@ -35,11 +59,48 @@ contract MainWallet is SimpleAccount {
     // Used to make reccuring payment
     function makeReccuringPayment(address _payeeWallet) public {
         require(isPaymentNeeded(_payeeWallet), "payment not made");
-        
+
         ReccuringPayeeInfo memory info = reccuringPayments[_payeeWallet];
         IERC20(info.token).transfer(_payeeWallet, info.paymentAmount);
         reccuringPayments[_payeeWallet].lastPaymentBlock += block.number;
+
+        // emit some event
     }
 
+    // only wallet owner can call
+    function setUpSocialRecovery(address _account1, address _account2, address _account3) external {
+        recoveryAccount1 = _account1;
+        recoveryAccount2 = _account2;
+        recoveryAccount3 = _account3;
+
+        // emit some event
+    }
+
+    // anyone can call
+    function recoverAccount() external {
+        // check attestationStation for signatures, saying this account has been lost
+        // from the attestations, derive the address of the new wallet
+        // set that wallet as newAccount
+    }
+
+    // anyone can call
+    function recoverWalletTokens(address[] calldata _tokens) external {
+        require(newAccount != address(0), "no recovery wallet was establised");
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            IERC20(_tokens).transfer(IERC20(_tokens).balanceOf(address(this)));
+        }
+    }
+
+    // anyone can call
+    function recoverWalletEth() external {
+        require(newAccount != address(0), "no recovery wallet was establised");
+        bool sent = newAccount.send(this.balance);
+        require(sent, "Failed to send Ether");
+    }
+
+    // function recoverWalletNFTs(address[] calldata _tokens) external {
+    //     require(newAccount != address(0), "no recovery wallet was establised");
+        
+    // }
 
 }
