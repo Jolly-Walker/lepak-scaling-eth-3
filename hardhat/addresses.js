6e273b6190eba77762fd5ca96cb60f44ff9a178b
Lim Hong Han <limhonghan1987@gmail.com>
2023-03-26 01:57:16 +0800

    Added password hashing

--- hardhat/addresses.js
+++ hardhat/addresses.js
@@ -1,10 +1,9 @@
-
 const addressList = {
-    attestationStation : "0xdce225e1247e3AF602d3541168521a94a053F4bc",
-    entryPoint : "0x0576a174D229E3cFA37253523E645A78A0C91B57",
-    automate : "0x255F82563b5973264e89526345EcEa766DB3baB2"
+	attestationStation: "0x1a1f1720A3a4CF7E1DE28434672e6b61643a943D",
+	entryPoint: "0x0576a174D229E3cFA37253523E645A78A0C91B57",
+	automate: "0x255F82563b5973264e89526345EcEa766DB3baB2",
 };
 
 module.exports = {
-    addressList
-  };
+	addressList,
+};
--- web/src/pages/api/getSimpleAccount.ts
+++ web/src/pages/api/getSimpleAccount.ts
@@ -1,47 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ethers } from "ethers";
-import { SimpleAccountAPI, PaymasterAPI } from "@account-abstraction/sdk";
+import { SimpleAccountAPI } from "@account-abstraction/sdk";
 
 const {
-  RPC_URL,
-  SIGNING_KEY,
-  ENTRY_POINT_ADDRESS: entryPointAddress,
-  SIMPLE_ACCOUNT_FACTORY_ADDRESS: factoryAddress,
+	RPC_URL,
+	ENTRY_POINT_ADDRESS: entryPointAddress,
+	SIMPLE_ACCOUNT_FACTORY_ADDRESS: factoryAddress,
 } = process.env;
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-  const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
-  const owner = new ethers.Wallet(SIGNING_KEY as string, provider);
-  const sw = new SimpleAccountAPI({
-    provider,
-    entryPointAddress,
-    owner,
-    factoryAddress,
-    PaymasterAPI,
-  });
+	const { password } = req.query;
+	const signingKey = ethers.utils
+		.keccak256(ethers.utils.toUtf8Bytes(password as string))
+		.slice(2);
 
-  sw.getUserOpReceipt = async (
-    userOpHash: string,
-    timeout = 3000,
-    interval = 5000
-  ): Promise<string | null> => {
-    const endtime = Date.now() + timeout;
-    const block = await sw.provider.getBlock("latest");
-    while (Date.now() < endtime) {
-      const events = await sw.entryPointView.queryFilter(
-        sw.entryPointView.filters.UserOperationEvent(userOpHash),
-        Math.max(0, block.number - 100)
-      );
-      if (events.length > 0) return events[0].transactionHash;
-      await new Promise((resolve) => setTimeout(resolve, interval));
-    }
-    return null;
-  };
+	const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+	const owner = new ethers.Wallet(signingKey, provider);
+	const sw = new SimpleAccountAPI({
+		provider,
+		entryPointAddress: entryPointAddress as string,
+		owner,
+		factoryAddress,
+	});
 
-  const addr = await sw.getCounterFactualAddress();
-  console.log("testing", addr);
-  res.status(200).json({ address: addr });
+	const addr = await sw.getCounterFactualAddress();
+	console.log("testing", addr, sw);
+	res.status(200).json({ address: addr });
 }
