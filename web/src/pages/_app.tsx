18e2a61016a731f172b67d76db5fbf3b6c11744c
Lim Hong Han <limhonghan1987@gmail.com>
2023-03-26 13:28:53 +0800

    Added context file

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,20 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
+import { WalletContextProvider } from "@/context/WalletContext";
 
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isSignedIn, setIsSignedIn] = useState(false);
+	const [isSignedIn, setIsSignedIn] = useState(false);
 
-  return (
-    <div className="bg-bg text-white text-lg font-medium">
-      <Navbar isSignedIn={isSignedIn} />
-      <main className="min-h-[calc(100vh-64px)]">
-        <div className="mx-auto p-4 xl:container lg:px-16 xl:px-20 md:p-8 pt-8">
-          <Component {...pageProps} />
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<>
+			<WalletContextProvider>
+				<div className="bg-bg text-white text-lg font-medium">
+					<Navbar isSignedIn={isSignedIn} />
+					<main className="min-h-[calc(100vh-64px)]">
+						<div className="mx-auto p-4 xl:container lg:px-16 xl:px-20 md:p-8 pt-8">
+							<Component {...pageProps} />
+						</div>
+					</main>
+				</div>
+			</WalletContextProvider>
+		</>
+	);
 }
