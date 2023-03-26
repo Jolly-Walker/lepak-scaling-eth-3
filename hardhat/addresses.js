aa4a8d74c2f889220dea4cdf6eb759dbfb731bf2
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-03-26 22:36:52 +0800

    chore: update addresses

--- hardhat/addresses.js
+++ hardhat/addresses.js
@@ -1,9 +1,9 @@
 const addressList = {
-	attestationStation: "0x1a1f1720A3a4CF7E1DE28434672e6b61643a943D",
-	entryPoint: "0x0576a174D229E3cFA37253523E645A78A0C91B57",
-	automate: "0x255F82563b5973264e89526345EcEa766DB3baB2",
+  attestationStation: "0x1a1f1720A3a4CF7E1DE28434672e6b61643a943D",
+  entryPoint: "0x0576a174D229E3cFA37253523E645A78A0C91B57",
+  automate: "0xB3f5503f93d5Ef84b06993a1975B9D21B962892F",
 };
 
 module.exports = {
-	addressList,
+  addressList,
 };
