1e32fc5186421880996f31cb7caa7fd8ff8fe223
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-03-26 22:36:47 +0800

    chore: update deploy script

--- hardhat/scripts/deployFactory.js
+++ hardhat/scripts/deployFactory.js
@@ -1,15 +1,26 @@
 const hre = require("hardhat");
-const {addressList} = require("../addresses.js");
+const { addressList } = require("../addresses.js");
 
 async function main() {
-    const walletFactoryDeployer = await ethers.getContractFactory('MainWalletFactory');
-    const walletFactory = await walletFactoryDeployer.deploy(addressList.entryPoint, addressList.attestationStation, addressList.automate);
-    await walletFactory.deployed();
-    console.log("wallet factory address deployed to:", walletFactory.address);
+  const { deployer } = await hre.getNamedAccounts();
+
+  console.log("Deploying contracts with the account:", deployer);
+  const signer = await hre.ethers.provider.getSigner(deployer);
+  const walletFactoryDeployer = await ethers.getContractFactory(
+    "MainWalletFactory"
+  );
+  const walletFactory = await walletFactoryDeployer
+    .connect(signer)
+    .deploy(
+      addressList.entryPoint,
+      addressList.attestationStation,
+      addressList.automate
+    );
+  await walletFactory.deployed();
+  console.log("wallet factory address deployed to:", walletFactory.address);
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
-  
+  console.error(error);
+  process.exitCode = 1;
+});
